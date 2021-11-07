2f74515a8482965f9b794d4d4e1d50a15691803d
RAJINIA <rajini1608@gmail.com>
2021-11-06 18:59:54 -0600

    BreedTest with Mocha and Chai

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,42 +1,39 @@
 const fetchBreedDescription = function(breedName, callback) {
   const request = require("request");
+  // Allow the user to specify the breed name using command-line arguments 
+  // (eg)  node breedFetcher.js Chartreux on terminal
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    console.log(typeof body);
+    
+    if (error) {
+      callback(error, undefined);
+      return;
+    }
+    const data = JSON.parse(body);
+    //console.log(data);          ===> gives the whole data of the URL
+    
+    // const description = data[0].description;     ====> DIFFERENT way to get the content of the description key
+    // const {description} = data[0];             ====> DIFFERENT way to get the content of the description key
+
+    //console.log(typeof data);
+    if (data.length === 0) {
+      callback("breed not found", undefined);
+    } else {
+          
+      const description = data[0]['description'];
+      callback(undefined, description);
+    }
+  });
+
 };
 
-// // Access the first entry in the data array and print out the description for the user.
-// const breedName = process.argv.slice(2)[0];         
-// //request('URL', callback function with (error, response, body) parameters)
-// request('https://api.thecatapi.com/v1/breeds/search?q=Siberian', (error, response, body) => {
-//   console.log(typeof body);
-//   const data = JSON.parse(body);
-//   //console.log(data);          ===> gives the whole data of the URL
-//   const description = data[0]['description'];
-//   // const description = data[0].description;     ====> DIFFERENT way to get the content of the description key
-//   // const {description} = data[0];             ====> DIFFERENT way to get the content of the description key
-
-//   console.log(typeof data);
-//   console.log(description);
-// })
 
 
 
 
 
 
-// const request = require("request");
-// // Allow the user to specify the breed name using command-line arguments 
-// // (eg)  node breedFetcher.js Chartreux on terminal
-// const breedName = process.argv.slice(2)[0];         
-// request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
-//   console.log(typeof body);
-//   const data = JSON.parse(body);
-//   //console.log(data);          ===> gives the whole data of the URL
-//   const description = data[0]['description'];
-//   // const description = data[0].description;     ====> DIFFERENT way to get the content of the description key
-//   // const {description} = data[0];             ====> DIFFERENT way to get the content of the description key
 
-//   console.log(typeof data);
-//   console.log(description);
-// })
 
 module.exports = { fetchBreedDescription };
 
--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const { fetchBreedDescription } = require('./breedFetcher');
 const breedName = process.argv[2];
 
 fetchBreedDescription(breedName, (error, desc) => {
+  //console.log("aaa");
   if (error) {
     console.log('Error fetch details:', error);
   } else {
     console.log(desc);
   }
 });
+
+console.log("zzz");
